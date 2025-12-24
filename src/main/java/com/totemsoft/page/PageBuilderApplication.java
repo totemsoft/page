1b677ab8e769a29882529b33dbf4e93aaf3cf7cc
shibaevv <shibaev.valera@gmail.com>
2025-12-24 13:21:21 +1100

    Initial import

---
+++ src/main/java/com/totemsoft/page/PageBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.totemsoft.page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/totemsoft/page/PageBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totemsoft.page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

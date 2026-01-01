ad8f58c9df98e12f335e969553a8ea3a984f0a66
shibaevv <shibaev.valera@gmail.com>
2026-01-01 15:45:31 +1100

    JPA (wip)

--- src/main/java/com/totemsoft/page/model/SeriesData.java
+++ src/main/java/com/totemsoft/page/model/SeriesData.java
@@ -5,7 +5,12 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
@@ -13,16 +18,23 @@ import lombok.Data;
 @Data
 @Builder
 @JsonInclude(Include.NON_NULL)
+@Entity
 public class SeriesData {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("id")
     private long id;
 
+    @JsonProperty("date")
     //@JsonFormat(shape = JsonFormat.Shape.NUMBER)
-    private LocalDate date;
+    private LocalDate seriesDate;
 
-    private BigDecimal value;
+    @JsonProperty("value")
+    private BigDecimal seriesValue;
 
     @NotBlank
+    @JsonProperty("title")
     private String title;
 
 }

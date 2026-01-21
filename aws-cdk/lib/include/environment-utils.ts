782557df5bb2966cd60d10d7cabfc06c9a1fa1f9
shibaevv <shibaev.valera@gmail.com>
2026-01-21 17:05:26 +1100

    minor

--- aws-cdk/lib/include/environment-utils.ts
+++ aws-cdk/lib/include/environment-utils.ts
@@ -4,6 +4,6 @@ export class EnvironmentUtils {
   static addEnvironments(id: string, containerDef: ContainerDefinition): void {
     containerDef.addEnvironment('PROFILE', id);
     containerDef.addEnvironment('TZ', 'Australia/Brisbane');
-    containerDef.addEnvironment('JAVA_OPTS', '-Xms1024m -Xmx1536m -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -Djava.net.preferIPv4Stack=true -Djboss.modules.system.pkgs=org.jboss.byteman -Djava.awt.headless=true');
+    containerDef.addEnvironment('JAVA_OPTS', '-Xms1024m -Xmx1536m -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -Djava.net.preferIPv4Stack=true -Djava.awt.headless=true');
   }
 }

f4e7cc2e5b0d02b1585a28c4b7878a8beb965e9d
shibaevv <shibaev.valera@gmail.com>
2026-01-23 16:59:40 +1100

    pagedb2

--- aws-cdk/lib/aws-cdk-stack.ts
+++ aws-cdk/lib/aws-cdk-stack.ts
@@ -49,7 +49,7 @@ export class AwsCdkStack extends cdk.Stack {
 //*/
     // EFS
     const efsVolumeName = 'efsVolume';
-    const efsMountPath = '/mnt/efs'; // '/usr/share'
+    const efsMountPath = '/mnt/efs';
 
     const domainName = props.domainName;
 

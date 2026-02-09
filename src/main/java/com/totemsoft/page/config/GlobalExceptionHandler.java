a93583a069f73c567b770f47189f00283bf9ef8f
shibaevv <shibaev.valera@gmail.com>
2026-02-09 13:11:35 +1100

    GlobalExceptionHandler

--- src/main/java/com/totemsoft/page/config/GlobalExceptionHandler.java
+++ src/main/java/com/totemsoft/page/config/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.totemsoft.page.config;
 import org.apache.tomcat.util.http.InvalidParameterException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -19,12 +20,18 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(Throwable.class)
     public final ResponseEntity<ErrorResponse> defaultErrorHandler(Throwable ex, WebRequest request) {
         final HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        return error(status, ex, "Unexpected Server ApiError");
+        return errorAndLog(status, ex, "Unexpected Server ApiError");
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponse> handle(EntityNotFoundException ex, WebRequest request) {
         final HttpStatus status = HttpStatus.NOT_FOUND;
+        return errorAndLog(status, ex, null);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handle(HttpRequestMethodNotSupportedException ex, WebRequest request) {
+        final HttpStatus status = HttpStatus.BAD_REQUEST;
         return error(status, ex, null);
     }
 
@@ -35,11 +42,15 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     private ResponseEntity<ErrorResponse> error(HttpStatus status, Throwable ex, String message) {
-        log.error(message, ex);
         final var error = new ErrorResponse(
                 status,
                 message == null ? ex.getMessage() : (message + ": " + ex.getMessage()));
         return new ResponseEntity<>(error, status);
+    }
+
+    private ResponseEntity<ErrorResponse> errorAndLog(HttpStatus status, Throwable ex, String message) {
+        log.error(message, ex);
+        return error(status, ex, message);
     }
 
 }
